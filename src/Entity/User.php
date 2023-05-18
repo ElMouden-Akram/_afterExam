2431806fdcfc6059e3b84099c31fdf29b9872012
ElMouden-Akram <akramelmouden15@gmail.com>
2023-05-18 18:10:54 +0100

    add user

---
+++ src/Entity/User.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $FirstName = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $LastName = null;
+
+    #[ORM\Column(length: 15, nullable: true)]
+    private ?string $CNI = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $CNE = null;
+
+    #[ORM\Column]
+    private ?bool $sexe = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $dateNaissance = null;
+
+    #[ORM\Column(length: 60,nullable: true)]
+    private ?string $adresse = null;
+
+    #[ORM\Column(nullable: true)]  #🚧 suprime nullable si tu veux que la'attribut sois obligatoir et lance un update de database!
+    private ?int $telephone = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->FirstName;
+    }
+
+    public function setFirstName(string $FirstName): self
+    {
+        $this->FirstName = $FirstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->LastName;
+    }
+
+    public function setLastName(string $LastName): self
+    {
+        $this->LastName = $LastName;
+
+        return $this;
+    }
+
+    public function getCNI(): ?string
+    {
+        return $this->CNI;
+    }
+
+    public function setCNI(?string $CNI): self
+    {
+        $this->CNI = $CNI;
+
+        return $this;
+    }
+
+    public function getCNE(): ?string
+    {
+        return $this->CNE;
+    }
+
+    public function setCNE(?string $CNE): self
+    {
+        $this->CNE = $CNE;
+
+        return $this;
+    }
+
+    public function isSexe(): ?bool
+    {
+        return $this->sexe;
+    }
+
+    public function setSexe(bool $sexe): self
+    {
+        $this->sexe = $sexe;
+
+        return $this;
+    }
+
+    public function getDateNaissance(): ?\DateTimeInterface
+    {
+        return $this->dateNaissance;
+    }
+
+    public function setDateNaissance(?\DateTimeInterface $dateNaissance): self
+    {
+        $this->dateNaissance = $dateNaissance;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?int
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(?int $telephone): self
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+}
